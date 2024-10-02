a1311d64f0cf10c5cfea2071a39817caff5b5d14
Alicia <aliciaji1993@gmail.com>
2024-10-02 11:44:23 +0000

    Move all dataset builders into /builders

---
+++ builders/go_stanford/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="go_stanford",
+    version="1.0.0",
+    packages=find_packages(where="."),
+    install_requires=[],
+    include_package_data=True,
+)
--- builders/rlds_dataset_builder.py
+++ builders/rlds_dataset_builder.py
@@ -12,45 +12,66 @@ import tensorflow_datasets as tfds
 import tensorflow_hub as hub
 
 from data_utils.convert_dataset import parse_trajectory
+from data_utils.gen_instruct.chat_wrapper import ChatGPT
+from data_utils.gen_instruct.generate import (
+    InstructType,
+    ContextType,
+    generate_instruction,
+)
+from data_utils.gen_instruct.template import INSTRUCT_TEMPLATES, INTRO_TEMPLATES
 
-CONFIG_FILE_PATH = "./process_data/config/dataset_config.yaml"
+
+CONFIG_FILE_PATH = (
+    "/home/yufeng/rlds_dataset_builder/data_utils/config/dataset_config.yaml"
+)
 ENCODER_PATH = (
     "/home/yufeng/.cache/tfhub_modules/google/universal-sentence-encoder-large/5"
 )
 
 
-class Sacson(tfds.core.GeneratorBasedBuilder):
+class RLDSDatasetBuilder(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for example dataset."""
 
-    VERSION = tfds.core.Version("2.0.0")
+    VERSION = tfds.core.Version("3.0.0")
     RELEASE_NOTES = {
         "1.0.0": "Initial release.",
         "2.0.0": "Actions predicting 8 future positions",
+        "3.0.0": "Actions predicting 8 future positions, plus language instruction",
     }
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, dataset_name: str, len_traj_pred: int = 8, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # configurable params
+        self.dataset_name = dataset_name
+        self.len_traj_pred = len_traj_pred
+
+        # embedding module
         # self._embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
-        # workaround:
+        # workaround (no access to tfhub.dev):
         # 1) manual download "https://hub.tensorflow.google.cn/google/universal-sentence-encoder-large/5"
         # 2) unzip and move to the tfhub cache folder
         # 3) load model directly from the cache folder by passing the path to hub.load
         self._embed = hub.load(ENCODER_PATH)
 
+        # language instruction generation
+        self.context_type = ContextType.OBS_1_ACTIONS_STRING
+        self.instruction_type = InstructType.FORMAT_ACTION
+        self.instructions = INSTRUCT_TEMPLATES[self.instruction_type]
+        self.chat = ChatGPT(system_prompt=INTRO_TEMPLATES[self.context_type])
+
+        # dataset configs
         with open(CONFIG_FILE_PATH, "r") as f:
             config = yaml.safe_load(f)
-
-        data_config = config["datasets"]["sacson"]
+        data_config = config["datasets"][dataset_name]
         self.data_folder = data_config["data_folder"]
         self.train_folder = data_config["train"]
         self.test_folder = data_config["test"]
-
         self.end_slack = data_config["end_slack"] if "end_slack" in data_config else 0
         self.waypoint_spacing = (
             data_config["waypoint_spacing"] if "waypoint_spacing" in data_config else 1
         )
         self.image_size = config["image_size"]
-        self.len_traj_pred = 8
 
     def _info(self) -> tfds.core.DatasetInfo:
         """Dataset metadata (homepage, citation,...)."""
@@ -98,6 +119,9 @@ class Sacson(tfds.core.GeneratorBasedBuilder):
                                 dtype=np.bool_,
                                 doc="True on last step of the episode if it is a terminal step, True for demos.",
                             ),
+                            "language_reasoning": tfds.features.Text(
+                                doc="Language Reasoning."
+                            ),
                             "language_instruction": tfds.features.Text(
                                 doc="Language Instruction."
                             ),
@@ -130,12 +154,8 @@ class Sacson(tfds.core.GeneratorBasedBuilder):
     def _generate_examples(self, split) -> Iterator[Tuple[str, Any]]:
         """Generator of examples for each split."""
 
-        # compute language embedding
-        language_instruction = "continue the trajectory"
-        language_embedding = self._embed([language_instruction])[0].numpy()
-
         def _parse_trajectory(traj_folder):
-            steps = parse_trajectory(
+            traj = parse_trajectory(
                 traj_folder=traj_folder,
                 image_size=self.image_size,
                 len_traj_pred=self.len_traj_pred,
@@ -144,19 +164,39 @@ class Sacson(tfds.core.GeneratorBasedBuilder):
 
             # assemble episode --> here we're assuming demos so we set reward to 1 at the end
             episode = []
-            for i, step in enumerate(steps):
+            traj_len = len(traj["images"])
+            for i in range(traj_len):
+                try:
+                    response = generate_instruction(
+                        chat=self.chat,
+                        images=traj["images"][i],
+                        actions=traj["actions"][i],
+                        instructions=self.instructions,
+                        context_type=self.context_type,
+                    )
+                    # compute lanuage embedding
+                    language_reasoning = response["reasoning"]
+                    language_instruction = response["instruction"]
+                    language_embedding = self._embed([language_instruction])[0].numpy()
+                except Exception as e:
+                    language_reasoning = ""
+                    language_instruction = "continue the trajectory"
+                    language_embedding = self._embed([language_instruction])[0].numpy()
+
+                # add step
                 episode.append(
                     {
                         "observation": {
-                            "image": step["image"],
-                            "state": step["state"].astype(np.float32),
+                            "image": traj["images"][i],
+                            "state": traj["states"][i].astype(np.float32),
                         },
-                        "action": step["action"],
+                        "action": traj["actions"][i],
                         "discount": 1.0,
-                        "reward": float(i == (len(steps) - 1)),
+                        "reward": float(i == (traj_len - 1)),
                         "is_first": i == 0,
-                        "is_last": i == (len(steps) - 1),
-                        "is_terminal": i == (len(steps) - 1),
+                        "is_last": i == (traj_len - 1),
+                        "is_terminal": i == (traj_len - 1),
+                        "language_reasoning": language_reasoning,
                         "language_instruction": language_instruction,
                         "language_embedding": language_embedding,
                     }
@@ -179,30 +219,12 @@ class Sacson(tfds.core.GeneratorBasedBuilder):
             traj_names = file_lines.split("\n")
         traj_names.remove("")
 
-        # filter invalid trajectories
-        # def validate_trajectory(traj_name):
-        #     if not traj_name:
-        #         return False
-        #     # check if trajectory folder exists
-        #     traj_folder = os.path.join(self.data_folder, traj_name)
-        #     if not os.path.exists(traj_folder):
-        #         print(f"Skipping non-existing trajectory {traj_name}...")
-        #         return False
-        #     # check if trajectory contains enough steps
-        #     traj_len = len(glob.glob(os.path.join(traj_folder, "*.jpg")))
-        #     if traj_len <= self.end_slack + self.len_traj_pred:
-        #         print(f"Skipping short trajectory {traj_name} of length {traj_len}...")
-        #         return False
-        #     return True
-
-        # traj_names = filter(validate_trajectory, traj_names)
-
         traj_folders = [os.path.join(self.data_folder, name) for name in traj_names]
 
         # for smallish datasets, use single-thread parsing
-        for traj_folder in tqdm.tqdm(traj_folders, disable=True, dynamic_ncols=True):
-            yield _parse_trajectory(traj_folder)
+        # for traj_folder in tqdm.tqdm(traj_folders, dynamic_ncols=True):
+        #     yield _parse_trajectory(traj_folder)
 
         # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
-        # beam = tfds.core.lazy_imports.apache_beam
-        # return beam.Create(traj_folders) | beam.Map(_parse_trajectory)
+        beam = tfds.core.lazy_imports.apache_beam
+        return beam.Create(traj_folders) | beam.Map(_parse_trajectory)
